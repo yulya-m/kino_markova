dd6b70f411f9e280cccc2807a02812a31a980fd7
Юля <markovayulya000@gmail.com>
2026-06-04 18:12:08 +0500

    Разработанные модели. Маркова

---
+++ Models/Clubs.cs
@@ -0,0 +1,10 @@
+﻿namespace Kino26_Markova.Models
+{
+    public class Clubs
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string WorkTime { get; set; }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,11 @@
+﻿namespace Kino26_Markova.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public DateTime RentStart { get; set; }
+        public int Duration { get; set; }
+        public int IdClub { get; set;}
+    }
+}
